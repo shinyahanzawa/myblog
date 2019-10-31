ab41f3b5c59aa34a12ed1ad6914ebf77093e2ad4
shinyahanzawa <sinya_02_28@yahoo.co.jp>
2019-11-01 07:29:31 +0900

    add

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,8 +12,14 @@
     @forelse($posts as $post)
     <li><a href="{{ action('PostsController@show', $post)}}">{{$post->title }}</a>
     <a href="{{ action('PostsController@edit', $post)}}" class="edit">[Edit]</a></li>
+    <a href="{{ action('PostsController@edit', $post)}}" class="del" data-id="{{ $post-id }}">[x]</a></li>
+    <form method = "post" action="{{ url('/posts',$post->id )}}" id="form_{{ $post->id }}">
+    {{ {{ csrf_field() }}
+    {{ method_field('delete') }}
+    </form> 
     @empty
     <li>No Posts yet</li>
     @endforelse
+    <script src="/js/main.js"></script>
 </ul>
 @endsection
