77080d0a8c547ab7d71f1aa9d67848141b090cb6
shinyahanzawa <sinya_02_28@yahoo.co.jp>
2019-11-02 07:57:37 +0900

    add

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,12 +11,13 @@
 <ul>
     @forelse($posts as $post)
     <li><a href="{{ action('PostsController@show', $post)}}">{{$post->title }}</a>
-    <a href="{{ action('PostsController@edit', $post)}}" class="edit">[Edit]</a></li>
-    <a href="{{ action('PostsController@edit', $post)}}" class="del" data-id="{{ $post-id }}">[x]</a></li>
+    <a href="{{ action('PostsController@edit', $post)}}" class="edit">[Edit]</a>
+    <a href="#" class="del" data-id="{{ $post->id }}">[x]</a>
     <form method = "post" action="{{ url('/posts',$post->id )}}" id="form_{{ $post->id }}">
-    {{ {{ csrf_field() }}
+    {{  csrf_field() }}
     {{ method_field('delete') }}
-    </form> 
+    </form>
+    </li>
     @empty
     <li>No Posts yet</li>
     @endforelse
